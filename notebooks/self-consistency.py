1a04c7ebbab2c5a5486b1e6011177606ba2ad318
gx xu <gxxu@redhat.com>
2025-10-01 10:36:09 -0400

    notebook update

--- notebooks/self-consistency.py
+++ notebooks/self-consistency.py
@@ -1,38 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     notebook_metadata_filter: all
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.2
-#   kernelspec:
-#     display_name: inference_time_scaling-dev
-#     language: python
-#     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 3
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython3
-#     version: 3.11.11
-# ---
-
-# %% [markdown]
-# # Self-Consistency Algorithm Demo
-# This notebook demonstrates the Self-Consistency algorithm for mathematical reasoning.
-
-# %%
-# %load_ext autoreload
-# %autoreload 2
-
-# %%
 import os
 from dotenv import load_dotenv
 from its_hub.utils import SAL_STEP_BY_STEP_SYSTEM_PROMPT
